5b9bfd340f1e7c152bdfbbe8872766bbfb75357f
Aaron Weiss <awe@pdgn.co>
2019-06-30 17:34:51 -0400

    annotated borrowck-assign-comp.rs

--- borrowck/borrowck-assign-comp.rs
+++ borrowck/borrowck-assign-comp.rs
@@ -1,8 +1,8 @@
 struct Point { x: isize, y: isize }
 
 fn a() {
-    let mut p = Point {x: 3, y: 4};
-    let q = &p;
+    let mut p: Point = Point {x: 3, y: 4};
+    let q: &'q Point = &p;
 
     // This assignment is illegal because the field x is not
     // inherently mutable; since `p` was made immutable, `p.x` is now
@@ -15,8 +15,8 @@ fn c() {
     // this is sort of the opposite.  We take a loan to the interior of `p`
     // and then try to overwrite `p` as a whole.
 
-    let mut p = Point {x: 3, y: 4};
-    let q = &p.y;
+    let mut p: Point = Point {x: 3, y: 4};
+    let q: &'q isize = &p.y;
     p = Point {x: 5, y: 7};//~ ERROR cannot assign to `p` because it is borrowed
     p.x; // silence warning
     *q; // stretch loan
@@ -26,8 +26,8 @@ fn d() {
     // just for completeness's sake, the easy case, where we take the
     // address of a subcomponent and then modify that subcomponent:
 
-    let mut p = Point {x: 3, y: 4};
-    let q = &p.y;
+    let mut p: Point = Point {x: 3, y: 4};
+    let q: &'q isize = &p.y;
     p.y = 5; //~ ERROR cannot assign to `p.y` because it is borrowed
     *q;
 }
