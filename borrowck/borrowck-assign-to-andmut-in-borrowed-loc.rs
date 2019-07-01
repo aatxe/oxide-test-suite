32884a351c9494ee65997d9e44c31e379f2d55d7
Aaron Weiss <awe@pdgn.co>
2019-07-01 08:49:12 -0400

    another borrowck test working as expected

--- borrowck/borrowck-assign-to-andmut-in-borrowed-loc.rs
+++ borrowck/borrowck-assign-to-andmut-in-borrowed-loc.rs
@@ -1,20 +1,20 @@
 // Test that assignments to an `&mut` pointer which is found in a
 // borrowed (but otherwise non-aliasable) location is illegal.
 
-struct S<'a> {
+struct SS<'a> {
     pointer: &'a mut isize
 }
 
-fn copy_borrowed_ptr<'a>(p: &'a mut S<'a>) -> S<'a> {
-    S { pointer: &mut *p.pointer }
+fn copy_borrowed_ptr<'a>(p: &'a mut SS<'a>) -> SS<'a> {
+    SS::<'a> { pointer: &mut *(*p).pointer }
 }
 
 fn main() {
-    let mut x = 1;
+    let mut x: u32 = 1;
 
     {
-        let mut y = S { pointer: &mut x };
-        let z = copy_borrowed_ptr(&mut y);
+        let mut y: SS<'a> = SS::<'a> { pointer: &mut x };
+        let z: SS<'a> = copy_borrowed_ptr::<'a>(&mut y);
         *y.pointer += 1;
         //~^ ERROR cannot use `*y.pointer`
         //~| ERROR cannot assign to `*y.pointer`
