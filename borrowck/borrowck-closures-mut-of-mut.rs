5cac6a11c3965d02172f1d697251189721ba274f
Aaron Weiss <awe@pdgn.co>
2019-07-09 10:54:47 -0400

    added annotations to borrowck-closures-mut-of-mut

--- borrowck/borrowck-closures-mut-of-mut.rs
+++ borrowck/borrowck-closures-mut-of-mut.rs
@@ -1,17 +1,19 @@
 // Tests that two closures cannot simultaneously both have mutable
 // access to the variable. Related to issue #6801.
 
-fn get(x: &isize) -> isize {
+fn get<'a>(x: &'a isize) -> isize {
     *x
 }
 
-fn set(x: &mut isize) {
+fn set<'a>(x: &'a mut isize) {
     *x = 4;
 }
 
-fn a(x: &mut isize) {
-    let mut c1 = || set(&mut *x);
-    let mut c2 = || set(&mut *x);
+fn a<'a>(x: &'a mut isize) {
+    let tmp0: &'b mut isize = &mut *x;
+    let mut c1: fn() -> () = || set::<'b>(tmp0);
+    let tmp1: &'c mut isize = &mut *x;
+    let mut c2: fn() -> () = || set::<'c>(tmp1);
     //~^ ERROR two closures require unique access to `x` at the same time
     c2(); c1();
 }
