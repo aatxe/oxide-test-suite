d9120e66a2bed89c9b735c28a769e9d276708109
Aaron Weiss <awe@pdgn.co>
2019-07-09 18:21:33 -0400

    annotated mut-of-imm example

--- borrowck/borrowck-closures-mut-of-imm.rs
+++ borrowck/borrowck-closures-mut-of-imm.rs
@@ -1,18 +1,20 @@
 // Tests that two closures cannot simultaneously have mutable
 // and immutable access to the variable. Issue #6801.
 
-fn get(x: &isize) -> isize {
+fn get<'a>(x: &'a isize) -> isize {
     *x
 }
 
-fn set(x: &mut isize) {
+fn set<'a>(x: &'a mut isize) {
     *x = 4;
 }
 
-fn a(x: &isize) {
-    let mut c1 = || set(&mut *x);
+fn a<'a>(x: &'a isize) {
+    let tmp0: &'b mut isize = &mut *x;
+    let mut c1 = || set::<'b>(tmp0);
     //~^ ERROR cannot borrow
-    let mut c2 = || set(&mut *x);
+    let tmp1: &'c mut isize = &mut *x;
+    let mut c2 = || set::<'c>(tmp1);
     //~^ ERROR cannot borrow
     //~| ERROR two closures require unique access to `x` at the same time
     c2(); c1();
