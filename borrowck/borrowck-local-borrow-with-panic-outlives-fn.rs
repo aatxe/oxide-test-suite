a10b560fb29661ddf329080241ffdcf061f55965
Aaron Weiss <awe@pdgn.co>
2019-07-10 20:56:09 -0400

    added gitignore for results.json, goldenfile for one test, and annotation for another

--- borrowck/borrowck-local-borrow-with-panic-outlives-fn.rs
+++ borrowck/borrowck-local-borrow-with-panic-outlives-fn.rs
@@ -1,5 +1,5 @@
 fn cplusplus_mode_exceptionally_unsafe(x: &mut Option<&'static mut isize>) {
-    let mut z = (0, 0);
+    let mut z: (isize, isize) = (0, 0);
     *x = Some(&mut z.1);
     //~^ ERROR `z.1` does not live long enough [E0597]
     panic!("catch me for a dangling pointer!")
