cb6ede6904754019651c400fd816d91f3c7ca2b9
Aaron Weiss <awe@pdgn.co>
2020-01-08 16:44:45 -0500

    changes to make borrowck-reborrow-from-shorter-lived-andmut compile

--- borrowck/borrowck-reborrow-from-shorter-lived-andmut.rs
+++ borrowck/borrowck-reborrow-from-shorter-lived-andmut.rs
@@ -1,21 +1,21 @@
 // Test that assignments to an `&mut` pointer which is found in a
 // borrowed (but otherwise non-aliasable) location is illegal.
 
-struct S<'a> {
+struct Sn<'a> {
     pointer: &'a mut isize
 }
 
-fn copy_borrowed_ptr<'a,'b>(p: &'a mut S<'b>) -> S<'b> {
-    S { pointer: &mut *p.pointer }
+fn copy_borrowed_ptr<'a,'b>(p: &'a mut Sn<'b>) -> Sn<'b> {
+    Sn::<'b> { pointer: &mut *(*p).pointer }
     //~^ ERROR lifetime mismatch
 }
 
 fn main() {
-    let mut x = 1;
+    let mut x: isize = 1;
 
     {
-        let mut y = S { pointer: &mut x };
-        let z = copy_borrowed_ptr(&mut y);
+        let mut y: Sn<'x> = Sn::<'x> { pointer: &mut x };
+        let z: Sn<'x> = copy_borrowed_ptr::<'y, 'x>(&mut y);
         *y.pointer += 1;
         *z.pointer += 1;
     }
