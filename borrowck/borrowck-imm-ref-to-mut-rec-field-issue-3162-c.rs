3a44cadb359333de6065d361df4302a921be26fe
Aaron Weiss <awe@pdgn.co>
2020-01-08 16:52:00 -0500

    added type annotations to borrowck-imm-ref-to-mut-rec-field-issue-3162-c

--- borrowck/borrowck-imm-ref-to-mut-rec-field-issue-3162-c.rs
+++ borrowck/borrowck-imm-ref-to-mut-rec-field-issue-3162-c.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let mut _a = 3;
-    let b = &mut _a;
+    let mut _a: u32 = 3;
+    let b: &'b mut u32 = &mut _a;
     {
-        let c = &*b;
+        let c: &'c u32 = &*b;
         _a = 4; //~ ERROR cannot assign to `_a` because it is borrowed
-        drop(c);
+        drop::<&'c u32>(c);
     }
-    drop(b);
+    drop::<&'b mut u32>(b);
 }
