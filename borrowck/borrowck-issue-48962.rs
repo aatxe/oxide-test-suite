df4b53d2ff946849fae1ff767443ab1673b0e526
Aaron Weiss <awe@pdgn.co>
2020-01-08 17:20:48 -0500

    type annotations for borrowck-issue-48962

--- borrowck/borrowck-issue-48962.rs
+++ borrowck/borrowck-issue-48962.rs
@@ -4,19 +4,19 @@ struct Node {
 }
 
 fn a() {
-    let mut node = Node {
+    let mut node: Node = Node {
         elem: 5,
         next: None,
     };
 
-    let mut src = &mut node;
-    {src};
+    let mut src: &'a mut Node = &mut node;
+    drop::<&'a mut Node>(src);
     src.next = None; //~ ERROR use of moved value: `src` [E0382]
 }
 
 fn b() {
-    let mut src = &mut (22, 44);
-    {src};
+    let mut src: &'b mut (u32, u32) = &mut (22, 44);
+    drop::<&'b mut (u32, u32)>(src);
     src.0 = 66; //~ ERROR use of moved value: `src` [E0382]
 }
 
