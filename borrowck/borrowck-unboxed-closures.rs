f041fbe6e7536f1a51f07c7ec695154290ca355b
Aaron Weiss <awe@pdgn.co>
2020-01-09 11:32:29 -0500

    type annotations for borrowck-unboxed-closures

--- borrowck/borrowck-unboxed-closures.rs
+++ borrowck/borrowck-unboxed-closures.rs
@@ -1,7 +1,7 @@
 fn a<F:Fn(isize, isize) -> isize>(mut f: F) {
-    let g = &mut f;
+    let g: &'a mut fn(isize, isize) -> isize = &mut f;
     f(1, 2);    //~ ERROR cannot borrow `f` as immutable
-    use_mut(g);
+    use_mut::<'a, fn(isize, isize) -> isize>(g);
 }
 fn b<F:FnMut(isize, isize) -> isize>(f: F) {
     f(1, 2);    //~ ERROR cannot borrow `f` as mutable, as it is not declared as mutable
@@ -14,4 +14,4 @@ fn c<F:FnOnce(isize, isize) -> isize>(f: F) {
 
 fn main() {}
 
-fn use_mut<T>(_: &mut T) { }
+fn use_mut<'a, T>(_: &'a mut T) { }
