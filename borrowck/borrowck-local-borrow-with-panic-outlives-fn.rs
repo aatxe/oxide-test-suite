cab7f1420492a6a1980fe1ee6157ee6e4a441b50
Aaron Weiss <awe@pdgn.co>
2020-01-09 11:30:52 -0500

    lifetime annotations for borrowck-local-borrow-with-panic-outlives-fn

--- borrowck/borrowck-local-borrow-with-panic-outlives-fn.rs
+++ borrowck/borrowck-local-borrow-with-panic-outlives-fn.rs
@@ -1,4 +1,4 @@
-fn cplusplus_mode_exceptionally_unsafe(x: &mut Option<&'static mut isize>) {
+fn cplusplus_mode_exceptionally_unsafe<'a>(x: &'a mut Option<&'static mut isize>) {
     let mut z: (isize, isize) = (0, 0);
     *x = Some(&mut z.1);
     //~^ ERROR `z.1` does not live long enough [E0597]
