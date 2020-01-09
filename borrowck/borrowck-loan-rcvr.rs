d8368f8ec0da9cd24900d105aff5e25d5bc0d225
Aaron Weiss <awe@pdgn.co>
2020-01-09 11:38:34 -0500

    type annotations and compilation fixes for borrowck-loan-rcvr

--- borrowck/borrowck-loan-rcvr.rs
+++ borrowck/borrowck-loan-rcvr.rs
@@ -1,37 +1,33 @@
 struct Point { x: isize, y: isize }
 
-trait Methods {
-    fn impurem(&self);
-    fn blockm<F>(&self, f: F) where F: FnOnce();
+fn impurem<'a>(p: &'a Point) {
 }
 
-impl Methods for Point {
-    fn impurem(&self) {
-    }
-
-    fn blockm<F>(&self, f: F) where F: FnOnce() { f() }
-}
+fn blockm<'a>(p: &'a Point, f: fn() -> ()) { f() }
 
 fn a() {
-    let mut p = Point {x: 3, y: 4};
+    let mut p: Point = Point {x: 3, y: 4};
 
     // Here: it's ok to call even though receiver is mutable, because we
     // can loan it out.
-    p.impurem();
+    let tmp0: &'a Point = &p;
+    impurem(tmp0);
 
     // But in this case we do not honor the loan:
-    p.blockm(|| { //~ ERROR cannot borrow `p` as mutable
+    let tmp1: &'b Point = &p;
+    blockm(tmp1, || { //~ ERROR cannot borrow `p` as mutable
         p.x = 10;
     })
 }
 
 fn b() {
-    let mut p = Point {x: 3, y: 4};
+    let mut p: Point = Point {x: 3, y: 4};
 
     // Here I create an outstanding loan and check that we get conflicts:
 
-    let l = &mut p;
-    p.impurem(); //~ ERROR cannot borrow
+    let l: &'l mut Point = &mut p;
+    let tmp0: &'a Point = &p;
+    impurem(tmp0); //~ ERROR cannot borrow
 
     l.x += 1;
 }
