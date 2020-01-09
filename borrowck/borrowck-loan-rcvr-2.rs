c5deaf4ad029fbc9a5143881243e57ff45e68da3
Aaron Weiss <awe@pdgn.co>
2020-01-09 11:39:32 -0500

    split borrowck-loan-rcvr into two tests

--- borrowck/borrowck-loan-rcvr-2.rs
+++ borrowck/borrowck-loan-rcvr-2.rs
@@ -5,21 +5,6 @@ fn impurem<'a>(p: &'a Point) {
 
 fn blockm<'a>(p: &'a Point, f: fn() -> ()) { f() }
 
-fn a() {
-    let mut p: Point = Point {x: 3, y: 4};
-
-    // Here: it's ok to call even though receiver is mutable, because we
-    // can loan it out.
-    let tmp0: &'a Point = &p;
-    impurem(tmp0);
-
-    // But in this case we do not honor the loan:
-    let tmp1: &'b Point = &p;
-    blockm(tmp1, || { //~ ERROR cannot borrow `p` as mutable
-        p.x = 10;
-    })
-}
-
 fn b() {
     let mut p: Point = Point {x: 3, y: 4};
 
