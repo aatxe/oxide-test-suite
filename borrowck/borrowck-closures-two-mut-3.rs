6514264e2776410e1e8786f695af6e1209ac76a5
Aaron Weiss <awe@pdgn.co>
2020-01-28 12:44:19 -0500

    pared down borrowck-closures-two-mut examples

--- borrowck/borrowck-closures-two-mut-3.rs
+++ borrowck/borrowck-closures-two-mut-3.rs
@@ -4,24 +4,8 @@
 
 fn to_fn_mut(f: fn() -> ()) -> fn() -> () { f }
 
-fn a() {
-    let mut x: usize = 3;
-    let c1: fn() -> () = to_fn_mut(|| x = 4);
-    let c2: fn() -> () = to_fn_mut(|| x = 5); //~ ERROR cannot borrow `x` as mutable more than once
-    drop::<(fn() -> (), fn() -> ())>((c1, c2));
-}
-
 fn set<'a>(x: &'a mut isize) {
     *x = 4;
-}
-
-fn b() {
-    let mut x: usize = 3;
-    let tmp0: &'t0 mut usize = &mut x;
-    let c1: fn() -> () = to_fn_mut(|| set::<'t0>(tmp0));
-    let tmp1: &'t1 mut usize = &mut x;
-    let c2: fn() -> () = to_fn_mut(|| set::<'t1>(tmp1)); //~ ERROR cannot borrow `x` as mutable more than once
-    drop::<(fn() -> (), fn() -> ())>((c1, c2));
 }
 
 fn c() {
@@ -29,31 +13,6 @@ fn c() {
     let c1: fn() -> () = to_fn_mut(|| x = 5);
     let tmp0: &'t0 mut usize = &mut x;
     let c2: fn() -> () = to_fn_mut(|| set::<'t0>(tmp0)); //~ ERROR cannot borrow `x` as mutable more than once
-    drop::<(fn() -> (), fn() -> ())>((c1, c2));
-}
-
-fn d() {
-    let mut x: usize = 3;
-    let c1: fn() -> () = to_fn_mut(|| x = 5);
-    let tmp0: &'t0 mut usize = &mut x;
-    let c2: fn() -> () = to_fn_mut(|| {
-        let _y: fn() -> () = to_fn_mut(|| set::<'t0>(tmp0));
-    }); // (nested closure)
-    //~^ ERROR cannot borrow `x` as mutable more than once
-    drop::<(fn() -> (), fn() -> ())>((c1, c2));
-}
-
-struct Foo {
-    f: Box<isize>
-}
-
-fn g() {
-    let mut x: Box<Foo> = Box::new(Foo { f: Box::new(3) });
-    let tmp0: &'t0 mut isize = &mut *(x.f);
-    let c1: fn() -> () = to_fn_mut(|| set::<'t0>());
-    let tmp1: &'t1 mut isize = &mut *(x.f);
-    let c2: fn() -> () = to_fn_mut(|| set::<'t1>());
-    //~^ ERROR cannot borrow `x` as mutable more than once
     drop::<(fn() -> (), fn() -> ())>((c1, c2));
 }
 
