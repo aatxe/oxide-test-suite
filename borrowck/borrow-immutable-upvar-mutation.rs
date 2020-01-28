51b84870b9d42fad0174bf0adcb277a09e034a57
Aaron Weiss <awe@pdgn.co>
2020-01-28 14:37:06 -0500

    fixed compilation of borrow-immutable-upvar-mutation

--- borrowck/borrow-immutable-upvar-mutation.rs
+++ borrowck/borrow-immutable-upvar-mutation.rs
@@ -3,33 +3,32 @@
 // Tests that we can't assign to or mutably borrow upvars from `Fn`
 // closures (issue #17780)
 
-fn set(x: &mut usize) { *x = 5; }
+fn set<'a>(x: &'a mut usize) { *x = 5; }
 
-fn to_fn<A,F:Fn<A>>(f: F) -> F { f }
-fn to_fn_mut<A,F:FnMut<A>>(f: F) -> F { f }
+fn to_fn(f: fn()) -> fn() { f }
 
 fn main() {
     // By-ref captures
-    {
-        let mut x = 0;
-        let _f = to_fn(|| x = 42); //~ ERROR cannot assign
+    let mut x: usize = 0;
+    let _f: fn() = to_fn(|| x = 42); //~ ERROR cannot assign
 
-        let mut y = 0;
-        let _g = to_fn(|| set(&mut y)); //~ ERROR cannot borrow
+    let mut y: usize = 0;
+    let tmp: &'t mut usize = &mut y;
+    let _g: fn() = to_fn(|| set::<'t>(tmp)); //~ ERROR cannot borrow
 
-        let mut z = 0;
-        let _h = to_fn_mut(|| { set(&mut z); to_fn(|| z = 42); }); //~ ERROR cannot assign
-    }
+    let mut z: usize = 0;
+    let tmp: &'t mut usize = &mut z;
+    let _h: fn() = to_fn(|| { set::<'t>(tmp); to_fn(|| z = 42); }); //~ ERROR cannot assign
 
     // By-value captures
-    {
-        let mut x = 0;
-        let _f = to_fn(move || x = 42); //~ ERROR cannot assign
+    let mut x: usize = 0;
+    let _f: fn() = to_fn(move || x = 42); //~ ERROR cannot assign
 
-        let mut y = 0;
-        let _g = to_fn(move || set(&mut y)); //~ ERROR cannot borrow
+    let mut y: usize = 0;
+    let tmp: &'t mut usize = &mut y;
+    let _g: fn() = to_fn(move || set::<'t>(tmp)); //~ ERROR cannot borrow
 
-        let mut z = 0;
-        let _h = to_fn_mut(move || { set(&mut z); to_fn(move || z = 42); }); //~ ERROR cannot assign
-    }
+    let mut z: usize = 0;
+    let tmp: &'t mut usize = &mut z;
+    let _h: fn() = to_fn(move || { set::<'t>(tmp); to_fn(move || z = 42); }); //~ ERROR cannot assign
 }
