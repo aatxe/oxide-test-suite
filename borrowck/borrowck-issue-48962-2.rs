0d49471a23634b82fba5256eaae3c44138da79c1
Aaron Weiss <awe@pdgn.co>
2020-01-29 13:24:33 -0500

    changed goldenfiles after new fixes

--- borrowck/borrowck-issue-48962-2.rs
+++ borrowck/borrowck-issue-48962-2.rs
@@ -7,7 +7,7 @@ fn b() {
     let mut tmp: (u32, u32) = (22, 44);
     let mut src: &'b mut (u32, u32) = &mut tmp;
     drop::<&'b mut (u32, u32)>(src);
-    src.0 = 66; //~ ERROR use of moved value: `src` [E0382]
+    (*src).0 = 66; //~ ERROR use of moved value: `src` [E0382]
 }
 
 fn main() {
