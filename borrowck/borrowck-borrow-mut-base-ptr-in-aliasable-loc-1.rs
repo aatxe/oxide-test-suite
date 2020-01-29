10be9ec26ff9e29e2b558551fee2f84b84518135
Aaron Weiss <awe@pdgn.co>
2020-01-29 18:02:15 -0500

    change inferred lifetimes in borrow-mut-base-ptr-in-aliasable-loc

--- borrowck/borrowck-borrow-mut-base-ptr-in-aliasable-loc-1.rs
+++ borrowck/borrowck-borrow-mut-base-ptr-in-aliasable-loc-1.rs
@@ -3,8 +3,8 @@
 //
 // Example from src/librustc_borrowck/borrowck/README.md
 
-fn foo<'a, 'b>(t0: &'a &'b mut isize) {
-    let t1: &'a &'b mut isize = t0;
+fn foo<'a>(t0: &'a &'a mut isize) {
+    let t1: &'a &'a mut isize = t0;
     let p: &'c isize = &**t0;
     **t1 = 22; //~ ERROR cannot assign
 }
