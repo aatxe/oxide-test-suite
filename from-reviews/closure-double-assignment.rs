5dc26d44cbcd46b657937e15bfe33228438406b8
Aaron Weiss <awe@pdgn.co>
2020-02-03 15:06:47 -0500

    fix bad typo annotation in closure-double-assignment

--- from-reviews/closure-double-assignment.rs
+++ from-reviews/closure-double-assignment.rs
@@ -1,7 +1,7 @@
 fn main() {
     let a: u32 = 0;
     let x: &'x mut u32 = &mut a;
-    let f: fn() -> () = |v: u32| { *x = v };
+    let f: fn(u32) -> () = |v: u32| { *x = v };
     // $\Gamma_C$ is again empty because nothing got consumed.
     // However, `x` and the reference captured by `f` alias, so this
     // should get rejected:
