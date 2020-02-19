51413363d6ee8c948385a42b06904420546042f0
Aaron Weiss <awe@pdgn.co>
2020-02-19 14:23:11 -0500

    more nll progress and unit range wrapper in issue-51512

--- nll/issue-51512.rs
+++ nll/issue-51512.rs
@@ -1,10 +1,7 @@
+struct UnitRange([u32; 1]);
+
 fn main() {
-    let range: [u32; 1] = [0];
-    let r: [u32; 1] = range;
-    let x: u32 = range[0];
-    // this example originally used ranges and errored, but
-    // the array desugaring turns them into something that implements Copy
-    //
-    // we could instead produce the same behavior by making a separate Range construct
-    // Ranges could be identical to Arrays but not implement Copy
+    let range: UnitRange = UnitRange([0]);
+    let r: UnitRange = range;
+    let x: u32 = range.0[0];
 }
--- nll/reference-carried-through-struct-field.rs
+++ nll/reference-carried-through-struct-field.rs
@@ -1,10 +1,11 @@
 struct Wrap<'a> { w: &'a mut u32 }
 
 fn foo() {
-    let mut x = 22;
-    let wrapper = Wrap { w: &mut x };
+    let mut x: u32 = 22;
+    let tmp0: &'t0 mut u32 = &mut x;
+    let wrapper: Wrap<'t0> = Wrap::<'t0> { w: tmp0 };
     x += 1; //~ ERROR cannot use `x` because it was mutably borrowed [E0503]
-    *wrapper.w += 1;
+    *(wrapper.w) += 1;
 }
 
 fn main() { }
