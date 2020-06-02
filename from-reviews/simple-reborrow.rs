b06fe1687b6f59f37eba0a993aba1bb9a14cf649
Aaron Weiss <awe@pdgn.co>
2020-06-02 14:10:20 -0400

    fixed from-reviews tests

--- from-reviews/simple-reborrow.rs
+++ from-reviews/simple-reborrow.rs
@@ -1,6 +1,6 @@
 struct Point(u32, u32);
 
 fn reborrow<'a>(pt: &'a mut Point) -> &'a mut u32 {
-    let r = #[lft = "a"] &mut (*pt).0;
+    let r = #[lft = "r"] &mut (*pt).0;
     r
 }
--- from-reviews/unnest.rs
+++ from-reviews/unnest.rs
@@ -1,3 +1,3 @@
 fn unnest<'a, 'b>(pt: &'a mut &'b mut u32) -> &'a mut u32 where 'b: 'a {
-    #[lft = "a"] &mut **pt
+    #[lft = "pt"] &mut **pt
 }
