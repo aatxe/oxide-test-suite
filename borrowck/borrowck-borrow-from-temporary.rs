1107064834923560951841522d990f0e366244d1
Aaron Weiss <awe@pdgn.co>
2020-06-15 19:45:05 -0400

    updated borrow-from-temporary annotations and goldenfile

--- borrowck/borrowck-borrow-from-temporary.rs
+++ borrowck/borrowck-borrow-from-temporary.rs
@@ -7,7 +7,7 @@ struct Foo(isize);
 
 fn foo<'a>() -> &'a isize {
     let tmp = id::<Foo>(Foo(3));
-    let x: &'x isize = &tmp.0;
+    let x = &tmp.0;
     x //~ ERROR cannot return value referencing temporary value
 }
 
